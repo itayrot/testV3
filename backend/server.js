b948792d3fea412db1e070972bc87dd4de84c19d
Itay Rotshtein <itayrot2584@gmail.com>
2024-09-10 20:40:46 +0300

    mongo connection

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect('mongodb://mongo:27017/testV2', {
+mongoose.connect('mongodb+srv://itayrot2584:itayrot2584@users.ra7dxz2.mongodb.net/?retryWrites=true&w=majority&appName=users', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+// mongoose.connect('mongodb://mongo:27017/testV2', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
 
 app.get('/', (req, res) => {
     res.send('API is working!');
